eeb98926e0d49a8c2999684c1083eeac2764de79
lewatoto <lewatoto@gmail.com>
2015-03-08 01:53:03 -0600

    obteniendo informacion por JSON y geolocalizacion

--- js/clima.js
+++ js/clima.js
@@ -1,6 +1,9 @@
-
+navigator.geolocation.getCurrentPosition(function(position) {
     var obj;
-    var enlace = "http://api.openweathermap.org/data/2.5/weather?q=Guatemala,gt";
+    var enlace = "http://api.openweathermap.org/data/2.5/weather?lat="+position.coords.latitude+"&lon="+position.coords.longitude;
+    alert(enlace);
     $.getJSON(enlace, function (datos){ //obtiene los datos y los guarda en la variable datos
-        alert(datos.sys.country);
+        alert(datos.name);
     });
+
+});
