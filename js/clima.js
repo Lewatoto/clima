58e2bfdfed57a5fad3f54263f3bfa6637659d151
lewatoto <lewatoto@localhost.localdomain>
2015-03-08 19:58:50 -0600

    agregando iconos y manejo

--- js/clima.js
+++ js/clima.js
@@ -2,12 +2,45 @@ navigator.geolocation.getCurrentPosition(function (position) {
     var lat = position.coords.latitude;
     var lon = position.coords.longitude;
     var enlace = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=" + lat + "&lon=" + lon + "&cnt=7&APPID=d17f9da8c5afba9cfe7f4f1c63b24a71";
-    alert(enlace);
+    //alert(enlace);
     $.getJSON(enlace, function (datos) { //obtiene los datos y los guarda en la variable datos
-        var temp = datos.list[0].temp.day - 273.15; //temperatura actual
+        // ==============================================================
+        // =                  Datos día actual                          =
+        // ==============================================================
+        
+        // Ingresando los datos a las variables
+        var temp_a = datos.list[0].temp.day - 273.15; //temperatura actual
+        var temp_min = datos.list[0].temp.min - 273.15; //temperatura minima del dia
+        var temp_max = datos.list[0].temp.max - 273.15; //temperatura maxima del dia
+        var cond = datos.list[0].weather[0].main; //descripción del estado lluvia, soleado, etc.
+        var cond_d = datos.list[0].weather[0].description; //descripción del estado mas detallado "el cielo está despejado".
+        var icon_d = datos.list[0].weather[0].icon; //indica que icono usar.
         alert(datos.city.name);
+        
+        // Mostrando los datos en la aplicación
         $("#ciudad").append(datos.city.name);
-        $("#temperatura").append(temp.toFixed(2)+"°C");
+        $("#temp_a").append(temp_a.toFixed(2)+"°C");
+        $("#temp_min").append(temp_min.toFixed(2)+"°C");
+        $("#temp_max").append(temp_max.toFixed(2)+"°C");
+        $("#descripcion").append(cond + ", " + cond_d);
+        var cambio = './img/svg/' + icon_d + '.svg';
+        alert(cambio);
+        $('#hoy_i').attr('src',cambio);
+        
+        // ==============================================================
+        // =                      Datos día 1                           =
+        // ==============================================================
+        
+        // ingresando los datos en variables
+        var temp_min_1 = datos.list[1].temp.min - 273.15; //temperatura minima del dia 1
+        var temp_max_1 = datos.list[1].temp.max - 273.15; //temperatura maxima del dia 1
+        var icon_d_1 = datos.list[0].weather[0].icon; //indica que icono usar.día 1
+        var cambio_1 = './img/svg/' + icon_d + '.svg';
+        
+        // Mostrando los datos
+        $("#min_1").append(temp_min_1.toFixed(2)+"°C");
+        $("#max_1").append(temp_max_1.toFixed(2)+"°C");
+        $("#dia_1_i").attr("src",cambio_1);
     });
 
 });
