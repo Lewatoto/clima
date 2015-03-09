9416c76a594126b70ae118b4e4586668aaef1644
lewatoto <lewatoto@gmail.com>
2015-03-09 17:28:30 -0600

    modificando css

--- js/clima.js
+++ js/clima.js
@@ -1,5 +1,23 @@
 var dias = new Array('Dom','Lun','Mar','Mie','Jue','Vie','Sab');
 var fecha = new Date();
+var hora_h = fecha.getHours();
+var p_dia;
+
+function fondo (hora){
+    if (hora >= 4 && hora < 8){
+        $('#estado_hoy').css({"background": "linear-gradient(to top, #234054, #0b2b42, #061927)"});
+    }
+    if (hora >= 8 && hora < 17){
+        $('#estado_hoy').css({"background": "linear-gradient(to top, #234054, #0b2b42, #061927)"});
+    }
+    if (hora >= 17 && hora < 19){
+        $('#estado_hoy').css({"background": "linear-gradient(to top, #234054, #0b2b42, #061927)"});
+    }
+    if (hora >= 19 && hora < 24){
+        $('#estado_hoy').css({"background": "linear-gradient(to top, #234054, #0b2b42, #061927)"});
+    }
+}
+
 
 navigator.geolocation.getCurrentPosition(function (position) {
     var lat = position.coords.latitude;
@@ -15,6 +33,7 @@ navigator.geolocation.getCurrentPosition(function (position) {
         var temp_a = datos.list[0].temp.day - 273.15; //temperatura actual
         var temp_min = datos.list[0].temp.min - 273.15; //temperatura minima del dia
         var temp_max = datos.list[0].temp.max - 273.15; //temperatura maxima del dia
+        var hum = datos.list[0].humidity; // humedad del día.
         var cond = datos.list[0].weather[0].main; //descripción del estado lluvia, soleado, etc.
         var cond_d = datos.list[0].weather[0].description; //descripción del estado mas detallado "el cielo está despejado".
         var icon_d = datos.list[0].weather[0].icon; //indica que icono usar.
@@ -25,6 +44,7 @@ navigator.geolocation.getCurrentPosition(function (position) {
         $("#temp_a").append(temp_a.toFixed(2)+"°C");
         $("#temp_min").append(temp_min.toFixed(2)+"°C");
         $("#temp_max").append(temp_max.toFixed(2)+"°C");
+        $("#humedad").append(hum + "%");
         $("#descripcion").append(cond + ", " + cond_d);
         var cambio = './img/svg/' + icon_d + '.svg';
         //alert(cambio);
@@ -41,10 +61,10 @@ navigator.geolocation.getCurrentPosition(function (position) {
         var cambio_1 = './img/svg/' + icon_d_1 + '.svg';
         
         // Mostrando los datos
-        $("#min_1").append(temp_min_1.toFixed(2)+"°C");
-        $("#max_1").append(temp_max_1.toFixed(2)+"°C");
+        $("#min_1").append(temp_min_1.toFixed(2)+"°C ");
+        $("#max_1").append(temp_max_1.toFixed(2)+"°C ");
         $("#dia_1_i").attr("src",cambio_1);
-        $("#dia_1n").append(dias[fecha.getDay()+1]);
+        $("#dia_1n").append(" " + dias[fecha.getDay()+1]);
         
         // ==============================================================
         // =                      Datos día 2                           =
@@ -60,14 +80,11 @@ navigator.geolocation.getCurrentPosition(function (position) {
         $("#min_2").append(temp_min_2.toFixed(2)+"°C");
         $("#max_2").append(temp_max_2.toFixed(2)+"°C");
         $("#dia_2_i").attr("src",cambio_2);
-        $("#dia_2n").append(dias[fecha.getDay()+2]);
+        $("#dia_2n").append(" " + dias[fecha.getDay()+2]);
         
         //cambiar el color de fondo
+        fondo(hora_h);
         
-        $('body').css({"background-color": "#0b2b42"},
-                      {"background": "@include filter-gradient(#0b2b42, #467ea3, vertical)"},
-                      {"background": "@include background-image(linear-gradient(top, #0b2b42 0%, #467ea3 100%))"}
-                     );
     });
 
 }, geo_options);
@@ -80,10 +97,11 @@ var geo_options = {
 
 
 
-alert
 
-
-//iconos del clima
-//https://github.com/erikflowers/weather-icons
-//http://merlinthered.deviantart.com/art/plain-weather-icons-157162192
-//https://github.com/jgillich/brackets-rpm
+/*
+http://www.color-hex.com/color/0b2b42
+https://developer.mozilla.org/en-US/docs/Web/CSS/linear-gradient
+https://developer.mozilla.org/en-US/docs/Web/CSS/border-radius
+https://developer.mozilla.org/en-US/docs/Web/CSS/Tools/Border-radius_generator
+http://www.cssmatic.com/es/gradient-generator#%27\-moz\-linear\-gradient\%28top\%2C\%20\%23e2e2e2\%200\%25\%2C\%20\%23dbdbdb\%2050\%25\%2C\%20\%23d1d1d1\%2051\%25\%2C\%20\%23fefefe\%20100\%25\%29\%3B%27
+*/
