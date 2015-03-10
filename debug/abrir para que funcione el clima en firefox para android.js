16ae79d194a8d71ef0d441315da575a3fe4edf2f
lewatoto <lewatoto@gmail.com>
2015-03-09 22:32:51 -0600

    agregando archivos para debug en firefox para android

--- debug/abrir para que funcione el clima en firefox para android.js
+++ debug/abrir para que funcione el clima en firefox para android.js
@@ -1,46 +1,14 @@
-var dias = new Array('Dom','Lun','Mar','Mie','Jue','Vie','Sab');
-var fecha = new Date();
-var hora_h = fecha.getHours();
-var p_dia;
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Ctrl+R),
+ * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
+ * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
+ */
+enlace="http://api.openweathermap.org/data/2.5/forecast/daily?lat=14.6211&lon=-90.5269&cnt=7&APPID=d17f9da8c5afba9cfe7f4f1c63b24a71&lang=sp";
 
-function fondo (hora){
-    if (hora >= 0 && hora < 5){
-        $('#estado_hoy').css({"background": "linear-gradient(to top, #0b2b42, #071e2e, #010507)"});
-    }
-    if (hora >= 4 && hora < 5){
-        $('#estado_hoy').css({"background": "linear-gradient(to top, #A09FA4, #586379, #27334B)"});
-    }
-    /*
-    if (hora >= 5 && hora < 6){
-        $('#estado_hoy').css({"background": "linear-gradient(to top, #FEB574, #F6B283, #BF9995)"});
-    }
-    */
-    if (hora >= 5 && hora < 6){
-        $('#estado_hoy').css({"background": "linear-gradient(to top, #FEB574, #F6B283, #BF9995)"});
-    }
-    if (hora >= 6 && hora < 8){
-        $('#estado_hoy').css({"background": "linear-gradient(to top, #A2AEC3, #90A1BD, #8796B7)"});
-    }
-    if (hora >= 8 && hora < 16){
-        $('#estado_hoy').css({"background": "linear-gradient(to top, #AAC3E1, #6893BE, #46759F)"});
-    }
-    if (hora >= 16 && hora < 17){
-        $('#estado_hoy').css({"background": "linear-gradient(to top, #E4AC8B, #A8898F, #656074)"});
-    }
-    if (hora >= 17 && hora < 19){
-        $('#estado_hoy').css({"background": "linear-gradient(to top, #2E3C5F, #192649, #080F2A)"});
-    }
-    if (hora >= 19 && hora < 24){
-        $('#estado_hoy').css({"background": "linear-gradient(to top, #0b2b42, #071e2e, #010507)"});
-    }
-}
-
-function clima (){
-    navigator.geolocation.getCurrentPosition(function (position) {
-        var lat = position.coords.latitude;
-        var lon = position.coords.longitude;
-        var enlace = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=" + lat + "&lon=" + lon + "&cnt=7&APPID=d17f9da8c5afba9cfe7f4f1c63b24a71&lang=sp";
-        //alert(enlace);
+alert(enlace);
         $.getJSON(enlace, function (datos) { 
             //obtiene los datos y los guarda en la variable datos
             // ==============================================================
@@ -104,30 +72,3 @@ function clima (){
             fondo(hora_h);
 
         });
-
-    }, errorCallback, geo_options);
-
-    var geo_options = {
-      enableHighAccuracy: true, 
-      maximumAge        : 60000, 
-      timeout           : 5000
-    };
-    
-    function errorCallback(error) {
-        alert('ERROR(' + error.code + '): ' + error.message);
-    };
-};
-
-
-
-$(window).load(function(){
-    clima();
-})
-
-/*
-http://www.color-hex.com/color/0b2b42
-https://developer.mozilla.org/en-US/docs/Web/CSS/linear-gradient
-https://developer.mozilla.org/en-US/docs/Web/CSS/border-radius
-https://developer.mozilla.org/en-US/docs/Web/CSS/Tools/Border-radius_generator
-http://www.cssmatic.com/es/gradient-generator#%27\-moz\-linear\-gradient\%28top\%2C\%20\%23e2e2e2\%200\%25\%2C\%20\%23dbdbdb\%2050\%25\%2C\%20\%23d1d1d1\%2051\%25\%2C\%20\%23fefefe\%20100\%25\%29\%3B%27
-*/
